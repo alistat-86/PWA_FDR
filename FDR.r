7b4e4c1f998f8de5295a36ff48ec0a74838f511c
ALI CHOO <128666889+alistat-86@users.noreply.github.com>
2023-11-03 11:47:43 +0800

    Add files via upload
    FDR (False Discovery Rate)is a statistical method used in multiple hypothesis testing to correct for multiple comparisons and control the expected proportion of false positives among the rejected hypotheses. In the context of pathway analysis, FDR is used to adjust p-values of the enriched pathways to account for the multiple testing problem, as you are testing multiple pathways simultaneously.

---
+++ FDR.r
@@ -0,0 +1,43 @@
+#FDR analysis for pathwasy analysis 
+#need to Load necessary libraries
+library(KEGGREST)
+library(clusterProfiler)
+library(org.Hs.eg.db)
+library(ggplot2)
+#######################################
+# Define function
+pathway_analysis <- function(gene_list, plot_bar = TRUE, plot_dot = TRUE) {
+  # Convert gene symbols to Entrez IDs
+  entrez_ids <- mapIds(org.Hs.eg.db, keys = gene_list, column = "ENTREZID", keytype = "SYMBOL")
+  
+  # Remove any potential NAs from entrez_ids
+  entrez_ids <- entrez_ids[!is.na(entrez_ids)]
+  
+  # Perform pathway analysis
+  enrich_result <- enrichKEGG(gene = entrez_ids, organism = "hsa", keyType = "kegg", pvalueCutoff = 0.05)
+  
+  # Calculate FDR-adjusted p-values
+  enrich_result@result$padj <- p.adjust(enrich_result@result$pvalue, method = "BH")
+  
+  # Plot bar plot if specified
+  if (plot_bar) {
+    bar_plot <- barplot(enrich_result, showCategory = 15, main = "KEGG PWA", ylab = "FDR")
+    print(bar_plot)
+  }
+  
+  # Plot dot plot if specified
+  if (plot_dot) {
+    dot_plot <- dotplot(enrich_result, showCategory = 15)
+    dot_plot <- dot_plot + ggtitle("KEGG PWA") + ylab("FDR")
+    print(dot_plot)
+  }
+  
+  # Return results
+  return(enrich_result@result)
+}
+
+# Example usage
+gene_list <- c("HLA-A01:01", "CD40LG", "IL1B", "TNF", "IFNG", "CXCL9", "CXCL10", "CXCR3", "CD27", "CD154", "TNFAIP3", "CXCL10", "IL6", "IL1B", "NFKB1", "JUN", "TNF", "FASLG", "TRAF1")
+results <- pathway_analysis(gene_list)
+print(results)
+####################################################################################################################################
